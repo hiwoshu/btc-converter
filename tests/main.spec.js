5f41dc9e142c8aa021e4ecb060b656b1b91a0420
Hiwoshu <hiwoshu@hotmail.com>
2019-03-07 18:20:18 -0300

    Create build

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,5 +1,15 @@
-import { expect } from 'chai';
+const expect = require('chai').expect;
 
-describe('Main', () => {
+const exec = require('child_process').exec;
+const btcConverter = 'node.exe ./src/main.js';
+const pkg = require('../package.json');
 
+describe('Main CLI', () => {
+  it('should return description when btc-converter --help', (done) => {
+    exec(`${btcConverter} --help`, (err, stdout, stderr) => {
+      if(err) throw err;
+      expect(stdout.includes('Convert Bitcoin to any currency defined.')).to.be.true;
+      done();
+    });
+  });
 });
